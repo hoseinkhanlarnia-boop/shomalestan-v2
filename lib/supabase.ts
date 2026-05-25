63daa5651f0fdebb71520a51d95721a25dd4ad16
hoseinkhanlarnia-boop <hosein.khanlarnia@gmail.com>
2026-05-25 15:14:59 +0330

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  'https://ilrulqnoxcibeeigqfoi.supabase.co',
+ 'sb_publishable_LvOqJuYFDMiYtOStyGqkZw_FEkqGzb_'
 )
 
 export const supabaseAdmin = createClient(
