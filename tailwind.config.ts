cfe193753bc749785577dd32a34eecf791ba2bd6
hoseinkhanlarnia-boop <hosein.khanlarnia@gmail.com>
2026-05-26 09:57:11 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss'
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
